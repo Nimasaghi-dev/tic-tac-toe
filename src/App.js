79a6f9562de8a78cd133b8a094c02776f06d174d
Nimasaghi-dev <nimasaghi.dev@gmail.com>
2024-12-22 17:53:18 +0100

    update-second-and-third-row

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]); 
-  const [player , setPlayer] = useState("X");
+  const [player , setPlayer] = useState("x");
 
   const chooseSquare = (square) => {
     setBoard(
@@ -16,11 +16,11 @@ function App() {
       })
     );
     if (player === "x") {
-      setPlayer("O");
+      setPlayer("o");
     } else {
-      setPlayer("X");
-    }
-  }
+      setPlayer("x");
+    }    
+  };
 
   return (
     <div className="App">
@@ -30,8 +30,16 @@ function App() {
           <Square val={board[1]} chooseSquare={() => {chooseSquare(1)}} />
           <Square val={board[2]} chooseSquare={() => {chooseSquare(2)}} />
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className='row'>
+          <Square val={board[3]} chooseSquare={() => {chooseSquare(3)}} />
+          <Square val={board[4]} chooseSquare={() => {chooseSquare(4)}} />
+          <Square val={board[5]} chooseSquare={() => {chooseSquare(5)}} />
+        </div>
+        <div className='row'>
+          <Square val={board[6]} chooseSquare={() => {chooseSquare(6)}} />
+          <Square val={board[7]} chooseSquare={() => {chooseSquare(7)}} />
+          <Square val={board[8]} chooseSquare={() => {chooseSquare(8)}} />
+        </div>
       </div>
     </div>
   );
