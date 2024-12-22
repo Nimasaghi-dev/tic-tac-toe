d38aa40ba098122a48ba462801379db5f54b3b8b
Nimasaghi-dev <nimasaghi.dev@gmail.com>
2024-12-22 16:23:38 +0100

    update-squares

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 function App() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]); 
 
+  const chooseSquare = (square) => {
+
+  }
 
   return (
     <div className="App">
       <div className='board'>
         <div className='row'>
-          <Square val={board[0]} chooseSquare={() => {alert(0)}} />
-          <Square val={board[1]} chooseSquare={() => {alert(1)}} />
-          <Square val={board[2]} chooseSquare={() => {alert(2)}} />
+          <Square val={board[0]} chooseSquare={() => {chooseSquare(0)}} />
+          <Square val={board[1]} chooseSquare={() => {chooseSquare(1)}} />
+          <Square val={board[2]} chooseSquare={() => {chooseSquare(2)}} />
         </div>
         <div className='row'></div>
         <div className='row'></div>
