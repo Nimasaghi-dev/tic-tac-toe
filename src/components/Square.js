5fa692c0a7d2482d2f58d00f88dd1d8e32b680c4
Nimasaghi-dev <nimasaghi.dev@gmail.com>
2024-12-22 16:02:39 +0100

    create-initial-component-for-squares

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+// this component will represent each square in the game board
+
+import React from "react";
+
+function Square ({val , chooseSquare  }) {
+    return (
+        <div>{val}</div>
+    )
+}
+
+export default Square;
