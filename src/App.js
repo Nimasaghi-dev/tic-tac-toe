3a882c9e208345f6e7ee29bae339726c3c57ae1d
Nimasaghi-dev <nimasaghi.dev@gmail.com>
2024-12-22 15:51:38 +0100

    initial-board-state-setup-add-style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+
+
   return (
     <div className="App">
-      
+      <div className='board'>
+
+      </div>
     </div>
   );
 }
